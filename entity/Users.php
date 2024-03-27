d3071d96fd9e602aec8a5dbe44380366c48ce299
avilovmaksim5 <avilovmaksim5@mail.ru>
2024-03-27 20:48:22 +0400

    update 27/03/24 added entity and repository

--- entity/Users.php
+++ entity/Users.php
@@ -2,6 +2,7 @@
 
 namespace app\entity;
 
+use app\repository\UsersRepository;
 use yii\web\IdentityInterface;
 
 /**
@@ -12,22 +13,31 @@ use yii\web\IdentityInterface;
  * @property string photo аватар пользователя
  *
  */
-*/
-
 class Users extends \yii\db\ActiveRecord implements IdentityInterface
 {
     public static function findIdentity($id)
     {
+        return new static(UsersRepository::getUserById($id));
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function validatePassword($password)
+    {
+        return password_verify($password, $this->password);
+    }
+
+    public function findUserByLogin($login)
+    {
+        return new static(UsersRepository::getUserByLogin($login));
     }
 
     public static function findIdentityByAccessToken($token, $type = null)
     {
         // TODO: Implement findIdentityByAccessToken() method.
-    }
-
-    public function getId()
-    {
-        // TODO: Implement getId() method.
     }
 
     public function getAuthKey()
---
+++ models/UserForm.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace app\models;
+
+use yii\base\Model as Model;
+
+class UserForm extends Model
+{
+    public $login;
+    public $password;
+    public $_user;
+
+    public function rules()
+    {
+        return [
+            [['login', 'password'], 'required'],
+            ['password', 'validatePassword'],
+//           ['login', 'label' => 'Логин'],
+//           ['password', 'label' => 'Пароль'],
+        ];
+    }
+
+    public function validatePassword($attributes, $params)
+    {
+        if (!$this->hasErrors()) {
+            $user = $this->getUser();
+            if (!$user || !$user->validatePassword($this->password)) {
+                $this->addError($attributes, "Ошибка в логине или пароле");
+            }
+        }
+    }
+
+    public function getUser()
+    {
+        if ($this->_user === false) {
+            $this->_user = User::findUserBylogin($this->login);
+        };
+        return $this->_user;
+    }
+
+    public function login()
+    {
+        if ($this->validate()) {
+            return \Yii::$app->user->login($this->getUser());
+        }
+        return false;
+    }
+}
